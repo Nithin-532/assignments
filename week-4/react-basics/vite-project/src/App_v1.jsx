0f9e1069e513bbe1dacaa465a8c1ea6faa14f7e6
Nithin-532 <kallurunithin8771@gmail.com>
2023-12-28 22:45:45 +0530

    week-4 completed

---
+++ week-4/react-basics/vite-project/src/App_v1.jsx
@@ -0,0 +1,103 @@
+// import { useState } from 'react'
+// import './App.css'
+
+// let globalId = 1;
+// let todoState = [];
+// let oldTodoState = [];
+
+// function addTodoToDom(todo) {
+//   const titleDiv = document.createElement("div");
+//   const descDiv = document.createElement("div");
+//   const parent = document.createElement("div");
+//   const button = document.createElement("button");
+//   titleDiv.innerHTML = todo.title;
+//   descDiv.innerHTML = todo.description;
+//   button.innerHTML = "Mark as Done";
+//   parent.setAttribute("id", todo.id);
+//   parent.appendChild(titleDiv);
+//   parent.appendChild(descDiv);
+//   parent.appendChild(button);
+//   const todos = document.getElementById("todos");
+//   todos.appendChild(parent);
+// }
+
+// function removeTodoFromDom(todo) {
+//   const parent = document.getElementById(todo.id);
+//   const todos = document.getElementById("todos");
+//   todos.removeChild(parent);
+// }
+
+// function updateTodoInDom(oldTodo, newTodo) {
+//   const parent = document.getElementById(oldTodo.id);
+//   parent.children[0].innerHTML = newTodo.title;
+//   parent.children[1].innerHTML = newTodo.description;
+// }
+
+// function updateState(newTodos) {
+//   // calculate the diff b/w newTodos and oldTodos.
+//   // More specifically, find out what todos are - 
+//   // 1. added
+//   // 2. deleted
+//   // 3. updated
+//   const added = [];
+//   const deleted = [];
+//   const updated = [];
+
+//   const oldMap = new Map();
+//   const newMap = new Map();
+
+//   oldTodoState.forEach(function(oldTodo) {
+//     oldMap.set(oldTodo.id, oldTodo);
+//   })
+
+//   newTodos.forEach(function(newTodo) {
+//     if (oldMap.has(newTodo.id)) {
+//       const oldTodo = oldMap.get(newTodo.id);
+//       if ((newTodo.title !== oldTodo.title) || (newTodo.description !== oldTodo.description)) {
+//         updated.push(newTodo);
+//         updateTodoInDom(oldTodo, newTodo);
+//       }
+//     } else {
+//       added.push(newTodo);
+//       addTodoToDom(newTodo);
+//     }
+//     newMap.set(newTodo.id, newTodo);
+//   })
+
+//   oldTodoState.forEach(function(oldTodo) {
+//     if (!newMap.has(oldTodo.id)) {
+//       removeTodoFromDom(oldTodo);
+//       deleted.push(oldTodo);
+//     }
+//   })
+//   oldTodoState = newTodos.slice();
+// }
+
+
+// function App() {
+//   const [count, setCount] = useState(0);
+
+//   function addTodo() {
+//     const title = document.getElementById("title").value;
+//     const description = document.getElementById("description").value;
+//     todoState.push({
+//       title: title,
+//       description: description,
+//       id: globalId++,
+//     })
+//     updateState(todoState);
+//   }
+
+//   return (
+//     <div>
+//       <input type="text" id="title" placeholder="Todo title"></input> <br /><br />
+//       <input type="text" id="description" placeholder="Todo description"></input> <br /><br />
+//       <button onClick={addTodo}>Add todo</button>
+//       <br /> <br />
+
+//       <div id="todos"></div>
+//     </div>
+//   )
+// }
+
+// export default App
